f4fd5e6be33b2d33eca4e703502d47b2d73bbdcd
Игорь Кот <iakot@outlook.com>
2022-06-16 11:58:51 +0700

    Docker and connect to db

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import ConfigService from './common/config/config.service.js';
 import {Container} from 'inversify';
 import {Component} from './types/component.types.js';
 import {ConfigInterface} from './common/config/config.interface.js';
+import {DatabaseInterface} from './common/database-client/database.interface.js';
+import DatabaseService from './common/database-client/database.service.js';
 
 const applicationContainer = new Container();
 applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
 applicationContainer.bind<LoggerService>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
+applicationContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
